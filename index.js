3ef5c808b5d2c6be306fdd90e281568b5410a843
Ganhatos <ganhatos@gmail.com>
2021-03-25 14:51:20 -0300

    Adicionando coisas pra caramba

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let pets = [
         peso: 3,
         tutor: 'Doug',
         contato: '(81) 98145-7456',
-        vacinado: true,
+        vacinado: false,
         servicos: ['banho', 'tosa']
     },
     {
@@ -20,8 +20,8 @@ let pets = [
         peso: 3,
         tutor: 'Doug',
         contato: '(81) 98145-7456',
-        vacinado: true,
-        servicos: ['banho']
+        vacinado: false,
+        servicos: []
     },
     {
         nome: 'Satanas',
@@ -49,4 +49,77 @@ const listarPets = () => {
     }
 }
 
+const vacinarPet = () => {
+    for (let pet of pets) {
+        if (pet.vacinado == false) {
+            pet.vacinado = true;
+            console.log(`${pet.nome} foi vacinado com sucesso!`)
+        }
+        else {
+            console.log(`Ops, ${pet.nome} já está vacinado!`)
+        }
+    }
+}
+
+const campanhaVacina = () => {
+    var soma = 0;
+    for (let pet of pets) {
+        if (pet.vacinado == false) {
+            soma ++;
+        }
+    }
+    console.log(`${soma} foram vacinados!`)
+}
+
+const adicionarPet = () => {
+    pets.push({
+        nome: 'Jk',
+        tipo: 'cachorro',
+        idade: 5,
+        raca: 'Vira-lata',
+        peso: 3,
+        tutor: 'Doug',
+        contato: '(81) 98145-7456',
+        vacinado: false,
+        servicos: []
+    });
+    for (let pet of pets) {
+        console.log(pet.nome);
+    }
+}
+
+adicionarPet();
+
+const darBanhoPet = () => {
+    for(let pet of pets){
+            if(!(pet.servicos.includes('banho'))){
+                pet.servicos.push('banho');
+                console.log(`${pet.nome} está de banho tomado!`);    
+        }
+    } 
+}
+
+darBanhoPet();
 listarPets();
+
+const tosarPet = () => {
+    for(let pet of pets){
+            if(!(pet.servicos.includes('tosa'))){
+                pet.servicos.push('tosa');
+                console.log(`${pet.nome} está com cabelinho na régua!`);    
+        }
+    } 
+}
+
+// tosarPet();
+
+const apararUnhasPet = () => {
+    for(let pet of pets){
+            if(!(pet.servicos.includes('corte de unhas'))){
+                pet.servicos.push('corte de unhas');
+                console.log(`${pet.nome} está de unhas aparadas!`);    
+        }
+    } 
+}
+
+// apararUnhasPet();
