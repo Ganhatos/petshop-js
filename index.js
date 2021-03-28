40658db6626a2b0389b6c3b93f9a6fed85472c16
Ganhatos <ganhatos@gmail.com>
2021-03-28 15:14:51 -0300

    Funções únicas e adicionado menu de opções para o prompt interligando as funções

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-let bancoDeDados = require('./dadosPet.json');
+const caminhoBancoDeDados = './dadosPet.json';
+let bancoDeDados = require(caminhoBancoDeDados);
 const moment = require('moment');
 const fs = require('fs');
+const prompt = require('prompt-sync')();
 const nomePetshop = "PETSHOP AVANADE";
-
 let pets = bancoDeDados.pets;
+
+const validarPet = (nomePet) => {
+    for (const pet of pets) {
+        if (pet.nome == nomePet) return true;
+    }
+}
+
+const indiceDoPet = (nomePet) => {
+    for (let i = 0; i < pets.length; i++) {
+        if (pets[i].nome == nomePet) {
+            return i;
+        }
+    }
+}
 
 const escreverJSON = () => {
     let dadosJS = JSON.stringify(bancoDeDados, null, 4);
 
-    fs.writeFileSync('dadosPet.json', dadosJS);
+    fs.writeFileSync(caminhoBancoDeDados, dadosJS);
 }
 
 const listarPets = () => {
@@ -27,24 +42,18 @@ const listarPets = () => {
 //listarPets();
 
 const vacinarPet = nomePet => {
-    for (let i = 0; i < pets.length; i++) {
-        if (pets[i].nome == nomePet) {
-            if (pets[i].vacinado == true) {
-                console.log(`Ops, ${pets[i].nome} já está vacinado!`);
-                return;
-            
-            } else {
-                pets[i].vacinado = true;
-                escreverJSON();
-                console.log(`${pets[i].nome} foi vacinado com sucesso!`);
-                return;
-            }
-        }       
+    if (validarPet(nomePet)) {
+        if (pets[indiceDoPet(nomePet)].vacinado == true) {
+            console.log(`Ops, ${pets[indiceDoPet(nomePet)].nome} já está vacinado!`);        
+        } else {
+            pets[indiceDoPet(nomePet)].vacinado = true;
+            escreverJSON();
+            console.log(`${pets[indiceDoPet(nomePet)].nome} foi vacinado com sucesso!`);
+        }     
+    } else {
+        console.log(`${nomePet} não foi encontrado!`);
     }
-    console.log(`${nomePet} não foi encontrado!`);
 }
-
-//vacinarPet('Sansão');
 
 const campanhaVacina = () => {
     var soma = 0;
@@ -57,54 +66,90 @@ const campanhaVacina = () => {
 }
 
 const adicionarPet = (nome, tipo, idade, raca, peso, tutor, contato, vacinado, servicos) => {    
-    let dados = {
-        nome: nome,
-        tipo: tipo,
-        idade: idade,
-        raca: raca,
-        peso: peso,
-        tutor: tutor,
-        contato: contato,
-        vacinado: vacinado,
-        servicos: servicos
-    };
-    
-    pets.push(dados);
+    if (!validarPet(nome)) {
+        let dados = {
+            nome: nome,
+            tipo: tipo,
+            idade: idade,
+            raca: raca,
+            peso: peso,
+            tutor: tutor,
+            contato: contato,
+            vacinado: vacinado,
+            servicos: servicos
+        };
+        
+        pets.push(dados);
 
-    escreverJSON();
-    
-    console.log(`${dados.nome} foi adicionado(a) com sucesso!`);
+        escreverJSON();
+        
+        console.log(`${dados.nome} foi adicionado(a) com sucesso!`);
+    } else {
+        console.log(`${nomePet} já está cadastrado, tente outro nome!`);
+    }
 }
-
-//adicionarPet("Cremosinho", "cachorro", 6, "Snoop", 12, "Alex", "(81)9 8541-7852", false, []);
 
 const atenderCliente = (servico, nomePet) => { 
-    for (let i = 0; i < pets.length; i++) {
-        if (pets[i].nome == nomePet) {
-            pets[i].servicos.push(JSON.stringify({
-                nome: servico,
-                data: moment().format('DD-MM-YYYY')
-            }));
-            //escreverJSON();
-        }       
-    }
+    if (validarPet(nomePet)) {
+        pets[indiceDoPet(nomePet)].servicos.push(JSON.stringify({
+            nome: servico,
+            data: moment().format('DD-MM-YYYY')
+        }));
+        
+        escreverJSON();
 
-    console.log(pets);
-
-    switch (servico) {
-        case 'banho':
-            console.log(`${nomePet} está de banho tomado!`);
-            break;
-        case 'tosa':
-            console.log(`${nomePet} está com cabelinho na régua!`);
-            break;
-        case 'corte de unhas':
-            console.log(`${nomePet} está de unhas aparadas!`);
-            break;
-        default:
-            console.log(`${nomePet} não foi encontrado!`);
-            break;
+        switch (servico) {
+            case 'banho':
+                console.log(`${nomePet} está de banho tomado!`);
+                break;
+            case 'tosa':
+                console.log(`${nomePet} está com cabelinho na régua!`);
+                break;
+            case 'corte de unhas':
+                console.log(`${nomePet} está de unhas aparadas!`);
+                break;
+        }
+    } else {
+        console.log(`${nomePet} não foi encontrado!`);
     }
 }
 
-//atenderCliente('banho', 'Sansão');
+var entrada = 9;
+
+while (entrada != 0) {
+    console.log(`
+    Bem-vindo ao ${nomePetshop}!
+    Estes são os serviços disponíveis: 
+    [1] - Listar Pets
+    [2] - Vacinar Pet
+    [3] - Saber quandos pets foram vacinados
+    [4] - Adicionar pet
+    [5] - Atender cliente
+    [0] - Sair`);
+    
+    entrada = prompt('Digite o número do serviço que deseja: ');
+    
+    if (entrada == '0') {
+        console.log('Volte sempre!');
+        break;
+    } 
+    else if (entrada == '1') listarPets();
+    else if (entrada == '2') vacinarPet(prompt('Digite o nome do Pet: '));
+    else if (entrada == '3') campanhaVacina();
+    else if (entrada == '4') {
+        let nome = prompt('Digite o nome do Pet: ');
+        let tipo = prompt('Digite o tipo de Pet: ');
+        let idade = prompt('Digite a idade do Pet: ');
+        let raca = prompt('Digite a raça do Pet: ');
+        let peso = prompt('Digite o peso do Pet: ');
+        let tutor = prompt('Digite o nome do tutor: ');
+        let contato = prompt('Digite o contato do tutor: ');
+        let vacinado = prompt('O pet está vacinado?[true/false] ');
+        adicionarPet(nome, tipo, idade, raca, peso, tutor, contato, vacinado, []);
+    }
+    else if (entrada == '5') {
+        let nome = prompt('Digite o nome do Pet: ');
+        let servico = prompt('Digite o tipo de serviço: ');
+        atenderCliente(servico, nome);
+    } else console.log('Opção inválida, tente novamente!');
+}
